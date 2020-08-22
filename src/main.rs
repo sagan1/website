56a12d2b7e69f2cad7faf96c255fe3740847b0a9
sagan <camjanssen120@gmail.com>
2020-08-22 15:55:20 -0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use rocket_contrib::serve::StaticFiles;
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")))
+        .launch();
 }
